27b99d3e769e0c1e888c3c499f1b7194774d4c3c
Caleb Smith <karagenit@outlook.com>
2018-08-12 09:15:12 -0400

    Split Main File

---
+++ lib/commit-time/github/user.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+require 'github/graphql'
+require 'commit-time'
+require 'date'
+
+##
+# Returns a CommitTime object from a given Github repo.
+# Commits are filtered such that only those authored by :author (which is,
+# by default, equal to :user) are saved. Passing "" for :author will *jankily*
+# calculate times for all users.
+#
+# TODO: rename user -> owner
+# TODO: use :dig more
+#
+def get_repo(token, user, repo, author: user)
+  query = File.read(__dir__ + '/repo.graphql')
+  vars = { user: user, repo: repo }
+
+  commits = []
+  continue = true
+  cursor = nil
+
+  while continue do
+    vars[:cursor] = cursor
+    result = Github.query(token, query, vars)
+    commits += result.dig("data", "repository", "defaultBranchRef", "target", "history", "edges").to_a
+    continue = result.dig("data", "repository", "defaultBranchRef", "target", "history", "pageInfo", "hasNextPage") || false
+    cursor = result.dig("data", "repository", "defaultBranchRef", "target", "history", "pageInfo", "endCursor")
+  end
+
+  # Empty repo with no commits or default branch
+  return nil if commits.nil?
+
+  commits.select! { |e| e.dig("node", "author", "user", "login") == author }
+  dates = commits.map { |e| e["node"]["authoredDate"] }
+  dates.map! { |date| DateTime.parse(date) }
+
+  CommitTime.new(dates)
+end
+
+##
+# TODO: use #dig more
+#
+def get_repo_list(token, user)
+  query = File.read(__dir__ + '/user.graphql')
+  vars = { user: user }
+
+  repos = []
+  continue = true
+  cursor = nil
+
+  while continue do
+    vars[:cursor] = cursor
+    result = Github.query(token, query, vars)
+    repos += result.dig("data", "user", "repositories", "edges").to_a
+    continue = result.dig("data", "user", "repositories", "pageInfo", "hasNextPage") || false
+    cursor = result.dig("data", "user", "repositories", "pageInfo", "endCursor")
+  end
+
+  repos.map { |e| { owner: e["node"]["owner"]["login"], name: e["node"]["name"] } }
+end
+
+def get_all_repos(token, user)
+  values = get_repo_list(token, user).map do |repo|
+    owner = repo[:owner]
+    name = repo[:name]
+    { name: "#{owner}/#{name}", times: get_repo(token, owner, name, author: user) }
+  end
+
+  # Remove entries where get_repo returned nil i.e. there was an issue with it
+  values.select { |val| !val[:times].nil? }
+end
